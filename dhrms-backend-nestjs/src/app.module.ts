7a53a7f95a173c79405c758b62bd5fcf7fe806eb
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-08-30 09:57:15 +0530

    chore: configure NestJS with Prisma

--- dhrms-backend-nestjs/src/app.module.ts
+++ dhrms-backend-nestjs/src/app.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DB_URL ?? 'postgresql://localhost:5432/dhrms',
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      autoLoadEntities: true,
-      synchronize: false,
-    }),
+    PrismaModule,
   ],
 })
 export class AppModule {}
