02500b466c90e119840bd302fd9344bece6729cb
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-08-30 09:46:00 +0530

    chore: add NestJS application entrypoint

---
+++ dhrms-backend-nestjs/src/main.ts
@@ -0,0 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: '*',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: false, transform: true }));
+  await app.listen(Number(process.env.PORT ?? 8080));
+}
+
+bootstrap();
