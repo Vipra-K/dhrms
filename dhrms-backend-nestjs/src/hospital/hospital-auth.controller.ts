264ecffcf4d49d07637ff149df8b5404c477b26f
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-08-30 10:13:01 +0530

    Add hospital registration controller

---
+++ dhrms-backend-nestjs/src/hospital/hospital-auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterHospitalDto } from './dto/register-hospital.dto';
+import { HospitalAuthService } from './hospital-auth.service';
+
+@Controller('api/hospitals')
+export class HospitalAuthController {
+  constructor(private readonly hospitalAuthService: HospitalAuthService) {}
+
+  @Post('register')
+  register(@Body() body: RegisterHospitalDto) {
+    return this.hospitalAuthService.registerHospital(body);
+  }
+}
