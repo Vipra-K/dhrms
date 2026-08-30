ced4966b7551d6df1426f8a1186d4123ff245f9b
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-08-30 10:03:11 +0530

    chore: register doctor module

--- dhrms-backend-nestjs/src/app.module.ts
+++ dhrms-backend-nestjs/src/app.module.ts
@@ -3,13 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { WorkerModule } from './worker/worker.module';
+import { DoctorModule } from './doctor/doctor.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    AuthModule,
-    WorkerModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, WorkerModule, DoctorModule],
 })
 export class AppModule {}
