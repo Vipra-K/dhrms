501a436665315053a0fb474f7c7894276e6c2e18
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-08-30 10:14:09 +0530

    Add medical module

---
+++ dhrms-backend-nestjs/src/medical/medical.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { MedicalController } from './medical.controller';
+import { MedicalService } from './medical.service';
+
+@Module({ controllers: [MedicalController], providers: [MedicalService], exports: [MedicalService] })
+export class MedicalModule {}
