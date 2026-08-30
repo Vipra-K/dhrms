2e63cda574ee1acf49c28d67fb4227e51c5fe9d1
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-08-30 14:28:00 +0530

    Add hospital doctor profile route

--- dhrms_frontend/src/App.jsx
+++ dhrms_frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import HospitalRegister from "./pages/HospitalRegister";
 import HospitalDashboard from "./pages/HospitalDashboard";
 import DoctorManagement from "./pages/hospital/DoctorManagement";
+import DoctorProfile from "./pages/hospital/DoctorProfile";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import DoctorDashboard from "./pages/doctor/DoctorDashboard";
 import WorkerManagement from "./pages/hospital/WorkerManagement";
@@ -21,24 +22,11 @@ const Protected = ({ role, children }) => <ProtectedRoute allowedRoles={[role]}>
 
 const App = () => <BrowserRouter><Routes>
   <Route path="/" element={<Home />} /><Route path="/home" element={<Home />} />
-  <Route path="/login" element={<RoleLoginPicker />} />
-  <Route path="/login/hospital" element={<RoleLogin role="HOSPITAL" />} />
-  <Route path="/login/doctor" element={<RoleLogin role="DOCTOR" />} />
-  <Route path="/login/worker" element={<RoleLogin role="WORKER" />} />
+  <Route path="/login" element={<RoleLoginPicker />} /><Route path="/login/hospital" element={<RoleLogin role="HOSPITAL" />} /><Route path="/login/doctor" element={<RoleLogin role="DOCTOR" />} /><Route path="/login/worker" element={<RoleLogin role="WORKER" />} />
   <Route path="/hospital/register" element={<HospitalRegister />} />
-  <Route path="/hospital" element={<Protected role="HOSPITAL"><HospitalDashboard /></Protected>} />
-  <Route path="/hospital/doctors" element={<Protected role="HOSPITAL"><DoctorManagement /></Protected>} />
-  <Route path="/hospital/workers" element={<Protected role="HOSPITAL"><WorkerManagement /></Protected>} />
-  <Route path="/hospital/workers/scan" element={<Protected role="HOSPITAL"><WorkerQrScanner /></Protected>} />
-  <Route path="/hospital/find-worker" element={<Protected role="HOSPITAL"><FindWorker /></Protected>} />
-  <Route path="/hospital/workers/:workerId" element={<Protected role="HOSPITAL"><WorkerProfile /></Protected>} />
-  <Route path="/doctor" element={<Protected role="DOCTOR"><DoctorDashboard /></Protected>} />
-  <Route path="/doctor/workers" element={<Protected role="DOCTOR"><MyWorkers /></Protected>} />
-  <Route path="/doctor/workers/:workerId" element={<Protected role="DOCTOR"><DoctorWorkerProfile /></Protected>} />
-  <Route path="/worker" element={<Protected role="WORKER"><WorkerDashboard /></Protected>} />
-  <Route path="/worker/profile" element={<Protected role="WORKER"><MyProfile /></Protected>} />
-  <Route path="/worker/medical-history" element={<Protected role="WORKER"><MedicalHistory /></Protected>} />
+  <Route path="/hospital" element={<Protected role="HOSPITAL"><HospitalDashboard /></Protected>} /><Route path="/hospital/doctors" element={<Protected role="HOSPITAL"><DoctorManagement /></Protected>} /><Route path="/hospital/doctors/:doctorId" element={<Protected role="HOSPITAL"><DoctorProfile /></Protected>} /><Route path="/hospital/workers" element={<Protected role="HOSPITAL"><WorkerManagement /></Protected>} /><Route path="/hospital/workers/scan" element={<Protected role="HOSPITAL"><WorkerQrScanner /></Protected>} /><Route path="/hospital/find-worker" element={<Protected role="HOSPITAL"><FindWorker /></Protected>} /><Route path="/hospital/workers/:workerId" element={<Protected role="HOSPITAL"><WorkerProfile /></Protected>} />
+  <Route path="/doctor" element={<Protected role="DOCTOR"><DoctorDashboard /></Protected>} /><Route path="/doctor/workers" element={<Protected role="DOCTOR"><MyWorkers /></Protected>} /><Route path="/doctor/workers/:workerId" element={<Protected role="DOCTOR"><DoctorWorkerProfile /></Protected>} />
+  <Route path="/worker" element={<Protected role="WORKER"><WorkerDashboard /></Protected>} /><Route path="/worker/profile" element={<Protected role="WORKER"><MyProfile /></Protected>} /><Route path="/worker/medical-history" element={<Protected role="WORKER"><MedicalHistory /></Protected>} />
   <Route path="*" element={<Navigate to="/" replace />} />
 </Routes></BrowserRouter>;
-
 export default App;
