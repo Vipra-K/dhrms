fe8029b1fabc2140ea0cf606020a72940100df82
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-08-30 10:02:04 +0530

    chore: register migrated NestJS modules

--- dhrms-backend-nestjs/src/app.module.ts
+++ dhrms-backend-nestjs/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { WorkerModule } from './worker/worker.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
+    AuthModule,
+    WorkerModule,
   ],
 })
 export class AppModule {}
