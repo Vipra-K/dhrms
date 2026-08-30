3434052766f2d8ef451ada3dee3f3d32fc011580
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-08-30 10:01:57 +0530

    chore: register worker self controller

--- dhrms-backend-nestjs/src/worker/worker.module.ts
+++ dhrms-backend-nestjs/src/worker/worker.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WorkerController } from './worker.controller';
+import { WorkerSelfController } from './worker-self.controller';
 import { WorkerQrService } from './worker-qr.service';
 import { WorkerService } from './worker.service';
 
 @Module({
-  controllers: [WorkerController],
+  controllers: [WorkerController, WorkerSelfController],
   providers: [WorkerService, WorkerQrService],
   exports: [WorkerService, WorkerQrService],
 })
