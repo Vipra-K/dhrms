1c091a1ff83fbe9297c5872ae35ec90e0263e083
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-09-07 22:16:46 +0530

    fix(frontend): remove duplicate case-variant medical service

--- dhrms_frontend/src/services/MedicalService.js
+++
@@ -1,7 +0,0 @@
-import api from "./api";
-
-export const getMedicalRecords = async (workerId) => (await api.get(`/doctors/me/workers/${workerId}/medical-records`)).data;
-export const createMedicalRecord = async (workerId, data) => (await api.post(`/doctors/me/workers/${workerId}/medical-records`, data)).data;
-export const updateMedicalRecord = async (recordId, data) => (await api.put(`/doctors/me/medical-records/${recordId}`, data)).data;
-export const createPrescription = async (recordId, data) => (await api.post(`/doctors/me/medical-records/${recordId}/prescriptions`, data)).data;
-export const updatePrescription = async (prescriptionId, data) => (await api.put(`/doctors/me/prescriptions/${prescriptionId}`, data)).data;
