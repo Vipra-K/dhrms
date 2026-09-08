1d804ed18a38fab0e1df31d60f99bc9e3b9ae124
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-09-08 12:54:03 +0530

    Add registration officer phone check API call

--- dhrms_frontend/src/services/registrationService.js
+++ dhrms_frontend/src/services/registrationService.js
@@ -3,3 +3,4 @@ import api from "./api";
 export const registerWorker = async (worker) => (await api.post("/registration/workers", worker)).data;
 export const getRegisteredWorkers = async (search = "") => (await api.get("/registration/workers", { params: search ? { search } : {} })).data;
 export const getRegisteredWorker = async (workerId) => (await api.get(`/registration/workers/${workerId}`)).data;
+export const checkWorkerPhone = async (phone) => (await api.get("/registration/workers/check-phone", { params: { phone } })).data;
