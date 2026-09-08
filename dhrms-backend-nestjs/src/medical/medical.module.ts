befff8318f942a6c7843c7e555ca2314c9136b7c
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-09-08 07:59:21 +0530

    Register Supabase storage service

--- dhrms-backend-nestjs/src/medical/medical.module.ts
+++ dhrms-backend-nestjs/src/medical/medical.module.ts
@@ -3,7 +3,11 @@ import { MedicalController } from './medical.controller';
 import { MedicalService } from './medical.service';
 import { MedicalAttachmentController } from './medical-attachment.controller';
 import { MedicalAttachmentService } from './medical-attachment.service';
-import { FirebaseStorageService } from './firebase-storage.service';
+import { SupabaseStorageService } from './supabase-storage.service';
 
-@Module({ controllers: [MedicalController, MedicalAttachmentController], providers: [MedicalService, MedicalAttachmentService, FirebaseStorageService], exports: [MedicalService] })
+@Module({
+  controllers: [MedicalController, MedicalAttachmentController],
+  providers: [MedicalService, MedicalAttachmentService, SupabaseStorageService],
+  exports: [MedicalService],
+})
 export class MedicalModule {}
