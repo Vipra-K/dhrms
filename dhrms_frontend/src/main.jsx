4450fd4192181b392ad747960c01f57ff8ab5f6c
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-09-08 13:58:37 +0530

    Load public auth UI fixes

--- dhrms_frontend/src/main.jsx
+++ dhrms_frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import "./index.css";
 import "./public.css";
+import "./public-fixes.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
