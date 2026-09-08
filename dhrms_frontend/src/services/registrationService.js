cc1754ad278b367d8f85fb15c5cac1b3ebd8a6f0
Vipra-K <149648847+Vipra-K@users.noreply.github.com>
2026-09-08 12:52:12 +0530

    Add registration officer worker list and profile API calls

--- dhrms_frontend/src/services/registrationService.js
+++ dhrms_frontend/src/services/registrationService.js
@@ -1,3 +1,5 @@
 import api from "./api";
 
 export const registerWorker = async (worker) => (await api.post("/registration/workers", worker)).data;
+export const getRegisteredWorkers = async (search = "") => (await api.get("/registration/workers", { params: search ? { search } : {} })).data;
+export const getRegisteredWorker = async (workerId) => (await api.get(`/registration/workers/${workerId}`)).data;
